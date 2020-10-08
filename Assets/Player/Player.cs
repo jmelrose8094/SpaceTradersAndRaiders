3df7b3f2e88cfa3ec13911b492c3ecb4cd68b899
jmelrose8094 <melrosj@sunypoly.edu>
2020-10-08 11:32:43 -0400

    Added more to the file hierarchy, added a button, and created player and game controller classes

---
+++ Assets/Player/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public string race;
+    public int power;
+    public int wealth;
+    public int achievement;
+
+    public bool trader;
+
+    public int numShips;
+
+
+    public Player(string r, bool t)
+    {
+        race = r;
+        trader = t;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Ship/p_ship.cs
+++ Assets/Ship/p_ship.cs
@@ -1,14 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
 using UnityEngine;
 
 public class p_ship : MonoBehaviour
 {
 
-
     public int numBeamWeapons;
     public [] p_beamWeapon arrBeamWeapons;
-    
+
     public int numGenerators;
     public [] p_sheildGenerators generators;
 
@@ -18,23 +19,20 @@ public class p_ship : MonoBehaviour
     public int numAntiMissile;
     public [] p_antiMissile antiMissile;
 
-    public int numCriticalHits;
+    public int numCriticalHits = 0;
     public int numArmor;
 
     public p_engine engine;
     public int maxComponents;
     public int slotsUsed;
 
-
-    // Start is called before the first frame update
-    void Start()
+    public p_ship()
     {
-
+        numBeamWeapons = 0;
+        numGenerators = 0;
+        numMissiles = 0;
+        numAntiMissiles = 0;
+        maxComponents = 5;
+        slotsUsed = 0;
+        
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-}
