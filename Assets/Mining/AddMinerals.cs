359d6fcb940a85a2f66d8163856656f01e0b2c84
Robert <robertsantoscruz@gmail.com>
2020-10-12 16:29:48 -0400

    Added scripts for mining and adding systems

---
+++ Assets/Mining/AddMinerals.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AddMinerals : MonoBehaviour
+{
+    public UnityEngine.UI.Text addMinText;
+    public GameObject greenSys;
+
+    private Text greenSysText;
+    public int greenSystems;
+    public int commonMinerals, rareMinerals, vRareMinerals;
+
+    public void Update()
+    {
+        greenSysText = greenSys.GetComponent<Text>();
+        greenSystems = int.Parse(greenSysText.text);
+    }
+    
+    
+    public void clicked()
+    {
+        commonMinerals += (greenSystems * 30);
+        rareMinerals += (greenSystems * 20);
+        vRareMinerals += (greenSystems * 10);
+        addMinText.text = "Common Minerals: " + commonMinerals + "\nRare Minerals: " + rareMinerals + "\nVery Rare Minerals: " + vRareMinerals;
+    }
+}
---
+++ Assets/Mining/AddSystem.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AddSystem : MonoBehaviour
+{
+    public UnityEngine.UI.Text sysNum;
+    public int sys = 0;
+    public int sysPerClick = 1;
+    
+    public void clicked()
+    {
+        sys += sysPerClick;
+        sysNum.text = " " + sys;
+    }
+}
