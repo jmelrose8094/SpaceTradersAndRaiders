3a4f1cf11f10428744ee40a41a2348540fd4dad1
jmelrose8094 <melrosj@sunypoly.edu>
2020-10-14 09:31:16 -0400

    Added some functionality to the end turn button
    There is still a bug where after the ships position is reset it moves to the position it was last at

---
+++ Assets/UI/End turn button/EndTurn.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndTurn : MonoBehaviour
+{
+
+    public Text Textfield;
+    public GameObject shipRef;
+    public Vector3 orgPos;
+    
+    //Basic function that sets the text of a text box
+    public void SetText(string text)
+    {
+        Textfield.text = text;
+
+    }
+
+    public void resetShip()
+    {
+        shipRef.transform.position = orgPos;
+    }
+
+    //public void IncTurn()
+    //{
+
+    //}
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        shipRef = GameObject.Find("p_ship");
+        orgPos = shipRef.transform.position;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(orgPos.x);
+        print(shipRef.transform.position.x);
+        
+    }
+}
