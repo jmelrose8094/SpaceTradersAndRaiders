07da4c654dc79b509483a40a9b93808df37e5465
Rushington <72104426+Rushington@users.noreply.github.com>
2020-10-28 20:35:52 -0400

    Title Screen

---
+++ Assets/Title Screen/MenuScreens.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Hosting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScreens : MonoBehaviour
+{
+    public string newGameScene;
+    public string creditScene;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(newGameScene);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    public void Credits()
+    {
+        SceneManager.LoadScene(creditScene);
+    }
+}
